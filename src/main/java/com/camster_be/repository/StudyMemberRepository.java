d8c36363e6c89e996b258f02d9f2638779862d50
KI HYUN YOON <116802387+spin54@users.noreply.github.com>
2024-09-22 19:18:38 +0900

    Update StudyMemberRepository.java

--- src/main/java/com/camster_be/repository/StudyMemberRepository.java
+++ src/main/java/com/camster_be/repository/StudyMemberRepository.java
@@ -1,9 +1,17 @@
 package com.camster_be.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.camster_be.entity.StudyMember;
 
 @Repository
 public interface StudyMemberRepository extends JpaRepository<StudyMember, Long>{
+	// 특정 스터디에 속한 모든 멤버 찾기
+    List<StudyMember> findByStudyId(Long studyId);
+
+    // 특정 스터디에서 특정 멤버 찾기
+    Optional<StudyMember> findByMemberIdAndStudyId(Long memberId, Long studyId);
 }
