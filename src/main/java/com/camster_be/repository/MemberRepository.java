5c5f4ed41639ce239e51baefc1640ce8826086d6
KI HYUN YOON <116802387+spin54@users.noreply.github.com>
2024-09-24 13:37:31 +0900

    Update MemberRepository.java

--- src/main/java/com/camster_be/repository/MemberRepository.java
+++ src/main/java/com/camster_be/repository/MemberRepository.java
@@ -1,10 +1,14 @@
 package com.camster_be.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.camster_be.entity.Member;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Long>{
-
+public interface MemberRepository extends JpaRepository<Member, Long> {
+	// 이메일로 사용자 찾기
+    Optional<Member> findByEmail(String email);
 }
+
