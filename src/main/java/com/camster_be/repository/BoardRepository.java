79497977c72bda74657e4d97cd713c52076ecb2b
KI HYUN YOON <116802387+spin54@users.noreply.github.com>
2024-09-24 22:25:55 +0900

    Update BoardRepository.java

--- src/main/java/com/camster_be/repository/BoardRepository.java
+++ src/main/java/com/camster_be/repository/BoardRepository.java
@@ -1,9 +1,14 @@
 package com.camster_be.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.camster_be.entity.Board;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long>{
+	// 스터디 ID로 게시물 목록 조회
+    List<Board> findByStudyId(Long studyId);
+    
 }
